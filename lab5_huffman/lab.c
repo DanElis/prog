76983774da3551ad09aed0e4e2c61e8ec4ec4413
Dan <e1iseev98@mail.ru>
2017-04-15 00:30:19 +0700

    refactoring queue

--- lab5_huffman/lab.c
+++ lab5_huffman/lab.c
@@ -1,28 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-typedef struct {
+typedef struct node{
 	char element;
 	int frequence;
+	struct node *next;
 }node;
 
+typedef struct {
+	node **head;
+	node **tail;
+}Queue;
 
 char read_data (int *frequency);
 char build_tree ();
-char build_queue (int *frequencym , int number);
-int queue_comp (const void *a, const void *b);
+void build_queue (int *frequencym , int number, Queue *queue);
+void push (int element, int frequence, node **head);
+
 int main (){
 	char t;
 	int *frequency = (int*)calloc(256, sizeof(int));
-	if(scanf("%c",t) != 1){
-		printf("error\n" );
-		return 0;
+	if (frequency == NULL){
+		printf("error\n");
+		return 1;
 	}
+
+	t = getchar();
+	read_data(frequency);
 	if (t == 'd'){
 
 	}
 	else if(t == 'c'){
-
+		read_data(frequency);
 	}
 	return 0;
 
@@ -31,36 +40,55 @@ int main (){
 char read_data (int *frequency){
 	char tmp;
 	int number = 256;
-	while ((tmp = getchar()) != EOF){
+	Queue *queue = NULL;
+	fprintf(stderr, "er\n" );
+	while ((tmp = getchar()) != '!'){
 		if (!frequency[tmp])
 			number--;
 		frequency[tmp]++;
 	}
 	number = 256 - number;
-	build_queue(frequency, number);
+	build_queue(frequency, number, queue);
 
 }
 
 
-char build_tree (){
+char build_tree (Queue *queue){
 
 	
 }
 
-char build_queue (int *frequency , int number){
-	node *queue = malloc(number*sizeof(node));
+void build_queue (int *frequency , int number, Queue *queue){
+	queue = malloc(sizeof(Queue*));
+	queue->head = malloc(sizeof(node**));
+	queue->tail = malloc(sizeof(node**));
+	*queue->head = NULL;
+	*queue->tail = NULL;
+
 	int j = 0;
-	for (int i = 0; i < 256; i++){
-		if (frequency[i]){
-			queue[j].element = i;
-			queue[j++].frequence = frequency[i];
+	int max = 0, element = 0;
+	for (int j = 0; j < number; j++){
+		max = 0;
+		for (int i = 0; i < 256; i++){
+			if (frequency[i] > max){
+				max = frequency[i];
+				element = i;
+			}
 		}
+		push(element, max, queue->head);
 	}
-	qsort(queue, number,sizeof(node), queue_comp);
-	
-	
+	build_tree(queue);
 }
 
-int queue_comp (const void *a, const void *b){
-	return (((node *)a)->frequence > ((node*)b)->frequence);
+void push (int element, int frequence, node **head){
+	node *tmp = NULL;
+	if((tmp = malloc(sizeof(node))) == NULL){
+		exit(1);
+	}
+	tmp->element = element;
+	tmp->frequence = frequence;
+	tmp->next = *head;
+	*head = tmp;
 }
+
+
